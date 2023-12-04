92aa809ce892191e8159f5c4734b3a0280657f89
tharwatsamy <tharwatsamy2004@gmail.com>
2023-12-04 19:08:14 +0200

    add adaptive layout widget

---
+++ lib/views/adaptive_layout_widget.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AdaptiveLayout extends StatelessWidget {
+  const AdaptiveLayout(
+      {super.key,
+      required this.mobileLayout,
+      required this.tabletLayout,
+      required this.desktopLayout});
+
+  final WidgetBuilder mobileLayout, tabletLayout, desktopLayout;
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constrains) {
+      if (constrains.maxWidth < 600) {
+        return mobileLayout(context);
+      } else if (constrains.maxWidth < 900) {
+        return tabletLayout(context);
+      } else {
+        return desktopLayout(context);
+      }
+    });
+  }
+}
---
+++ lib/views/dashboard_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DashBoradView extends StatelessWidget {
+  const DashBoradView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
