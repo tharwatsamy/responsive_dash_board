b9a4a2cf52ad84e2b4fceca6a58fd636d4c41d3a
tharwatsamy <tharwatsamy2004@gmail.com>
2023-12-09 16:08:41 +0200

    create custom background container

--- lib/widgets/all_expensess.dart
+++ lib/widgets/all_expensess.dart
@@ -4,21 +4,15 @@ import 'package:responsive_dash_board/utils/app_images.dart';
 import 'package:responsive_dash_board/widgets/all_expensess_header.dart';
 import 'package:responsive_dash_board/widgets/all_expensess_items_list_view.dart';
 import 'package:responsive_dash_board/widgets/all_expenssess_item.dart';
+import 'package:responsive_dash_board/widgets/custom_background_container.dart';
 
 class AllExpenses extends StatelessWidget {
   const AllExpenses({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(20),
-      decoration: ShapeDecoration(
-        color: Colors.white,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12),
-        ),
-      ),
-      child: const Column(
+    return const CustomBackgroundContainer(
+      child: Column(
         children: [
           AllExpensessHeader(),
           SizedBox(
---
+++ lib/widgets/custom_background_container.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class CustomBackgroundContainer extends StatelessWidget {
+  const CustomBackgroundContainer({
+    super.key,
+    required this.child,
+    this.padding,
+  });
+
+  final Widget child;
+  final double? padding;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(padding ?? 20),
+      decoration: ShapeDecoration(
+        color: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+      ),
+      child: child,
+    );
+  }
+}
