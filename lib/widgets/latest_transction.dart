425b1848654c92425003e7b34d7c71139bac0801
tharwatsamy <tharwatsamy2004@gmail.com>
2023-12-09 22:53:24 +0200

    latest transction part one

---
+++ lib/widgets/latest_transction.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/utils/app_styles.dart';
+import 'package:responsive_dash_board/widgets/latest_transction_list_view.dart';
+
+class LatestTransction extends StatelessWidget {
+  const LatestTransction({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        Text(
+          'Latest Transaction',
+          style: AppStyles.styleMedium16,
+        ),
+        SizedBox(
+          height: 16,
+        ),
+        LatestTransctionListView()
+      ],
+    );
+  }
+}
---
+++ lib/widgets/latest_transction_list_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LatestTransctionListView extends StatelessWidget {
+  const LatestTransctionListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
