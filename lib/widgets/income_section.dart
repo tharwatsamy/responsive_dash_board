5968f3319caff200a032478904b2330f3f429734
tharwatsamy <tharwatsamy2004@gmail.com>
2023-12-18 08:31:54 +0200

    income chart

--- lib/widgets/income_section.dart
+++ lib/widgets/income_section.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/utils/app_styles.dart';
 import 'package:responsive_dash_board/widgets/custom_background_container.dart';
+import 'package:responsive_dash_board/widgets/income_chart.dart';
 import 'package:responsive_dash_board/widgets/income_section_header.dart';
 
 class IncomeSection extends StatelessWidget {
@@ -12,6 +13,13 @@ class IncomeSection extends StatelessWidget {
         child: Column(
       children: [
         IncomeSectionHeader(),
+        Expanded(
+          child: Row(
+            children: [
+              Expanded(child: IncomeChart()),
+            ],
+          ),
+        )
       ],
     ));
   }
