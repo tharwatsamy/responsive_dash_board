aa017dc64edbc5f3e62b01b2ad3d4e1153b28118
tharwatsamy <tharwatsamy2004@gmail.com>
2023-12-24 09:29:45 +0200

    mobile layout done

--- lib/views/dashboard_view.dart
+++ lib/views/dashboard_view.dart
@@ -1,23 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/adaptive_layout_widget.dart';
+import 'package:responsive_dash_board/widgets/custom_drawer.dart';
 import 'package:responsive_dash_board/widgets/dashboard_desktop_layout.dart';
 import 'package:responsive_dash_board/widgets/dashboard_mobile_layout.dart';
 import 'package:responsive_dash_board/widgets/dashboard_tablet_layout.dart';
 
-class DashBoradView extends StatelessWidget {
+class DashBoradView extends StatefulWidget {
   const DashBoradView({super.key});
 
   @override
+  State<DashBoradView> createState() => _DashBoradViewState();
+}
+
+class _DashBoradViewState extends State<DashBoradView> {
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldKey,
       appBar: MediaQuery.sizeOf(context).width < 800
           ? AppBar(
               elevation: 0,
               backgroundColor: const Color(0xFFFAFAFA),
-              leading: const Icon(Icons.menu),
+              leading: IconButton(
+                  onPressed: () {
+                    scaffoldKey.currentState!.openDrawer();
+                  },
+                  icon: const Icon(Icons.menu)),
             )
           : null,
       backgroundColor: const Color(0xFFF7F9FA),
+      drawer:
+          MediaQuery.sizeOf(context).width < 800 ? const CustomDrawer() : null,
       body: AdaptiveLayout(
         mobileLayout: (context) => const DashBoardMobileLayout(),
         tabletLayout: (context) => const DashBoardTabletLayout(),
--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -13,6 +13,7 @@ class CustomDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: MediaQuery.sizeOf(context).width * .7,
       color: const Color.fromRGBO(255, 255, 255, 1),
       child: const CustomScrollView(
         slivers: [
